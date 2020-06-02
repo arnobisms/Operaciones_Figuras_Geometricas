162b47665ee1ffacb890fc2a2028570400f899b7
Arnobis Melendez <arnobis2000@gmail.com>
2020-06-02 01:40:46 -0500

    Interfaces de la opcion Area terminadas, faltan las de cada figura de Volumenes

--- app/src/main/java/com/example/operaciones/SeleccionarArea.java
+++ app/src/main/java/com/example/operaciones/SeleccionarArea.java
@@ -2,7 +2,10 @@ package com.example.operaciones;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -11,6 +14,7 @@ public class SeleccionarArea extends AppCompatActivity {
     private ListView lista_areas;
     private String[] opciones_area;
     private ArrayAdapter<String> adapter;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +25,33 @@ public class SeleccionarArea extends AppCompatActivity {
         opciones_area = getResources().getStringArray(R.array.opciones_area);
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, opciones_area);
         lista_areas.setAdapter(adapter);
+        lista_areas.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                switch (position){
+                    case 0:
+                        intent = new Intent(SeleccionarArea.this, SeleccionarAreaCuadrado.class);
+                        startActivity(intent);
+                        break;
+
+                    case 1:
+                        intent = new Intent(SeleccionarArea.this, SeleccionarAreaRectangulo.class);
+                        startActivity(intent);
+                        break;
+
+                    case 2:
+                        intent = new Intent(SeleccionarArea.this, SeleccionarAreaTriangulo.class);
+                        startActivity(intent);
+                        break;
+
+                    case 3:
+                        intent = new Intent(SeleccionarArea.this, SeleccionarAreaCirculo.class);
+                        startActivity(intent);
+                        break;
+                }
+            }
+        });
 
 
     }
