9629a949e79a7d295e25b053cdb70a9e24df54b7
Arnobis Melendez <arnobis2000@gmail.com>
2020-06-02 22:14:29 -0500

    Interfaz terminada bilingue

--- app/src/main/java/com/example/operaciones/SeleccionarVolumen.java
+++ app/src/main/java/com/example/operaciones/SeleccionarVolumen.java
@@ -2,7 +2,10 @@ package com.example.operaciones;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -11,6 +14,7 @@ public class SeleccionarVolumen extends AppCompatActivity {
     private ListView lista_volumenes;
     private String[] opciones_volumen;
     private ArrayAdapter<String> adapter;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +25,34 @@ public class SeleccionarVolumen extends AppCompatActivity {
         opciones_volumen = getResources().getStringArray(R.array.opciones_volumen);
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,opciones_volumen);
         lista_volumenes.setAdapter(adapter);
+        lista_volumenes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                switch (position){
+
+                    case 0:
+                      intent = new Intent(SeleccionarVolumen.this, SeleccionarVolumenEsfera.class);
+                      startActivity(intent);
+                        break;
+
+                    case 1:
+                      intent = new Intent(SeleccionarVolumen.this, SeleccionarVolumenCilindro.class);
+                      startActivity(intent);
+                        break;
+
+                    case 2:
+                        intent = new Intent(SeleccionarVolumen.this, SeleccionarVolumenCono.class);
+                        startActivity(intent);
+                        break;
+
+                    case 3:
+                        intent = new Intent(SeleccionarVolumen.this, SeleccionarVolumenCubo.class);
+                        startActivity(intent);
+                        break;
+                }
+            }
+        });
 
     }
 }
